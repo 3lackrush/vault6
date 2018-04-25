9d8f1acf8b27634ce675b50de2d0457df7f9b009
3lackRush <505478266@qq.com>
2018-04-25 18:24:31 +0800

    add utils

---
+++ utils/banner.py
@@ -0,0 +1,15 @@
+__author__ = 'Kios'
+__description__ = "Vault6 is made to protect your data"
+
+Banner = '''
+__     __          _ _    __   
+\ \   / /_ _ _   _| | |_ / /_  
+ \ \ / / _` | | | | | __| '_ \ 
+  \ V / (_| | |_| | | |_| (_) |
+   \_/ \__,_|\__,_|_|\__|\___/ 
+                               
+'''
+
+Note = '''
+NOTE! Generate KEYS FIRST
+'''
